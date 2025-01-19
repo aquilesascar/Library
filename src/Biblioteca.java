f45221ece04668ee031cca51777a606a7be4c5c8
Estella1 <estella.santos.moreira@gmail.com>
2025-01-19 20:30:04 -0300

    carregar o arquivo

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -34,35 +35,6 @@ public class Biblioteca {
         }
     }
 
-    public void menuUsuario() {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("\nMenu Principal");
-            System.out.println("1. Consultar obras");
-            System.out.println("2. Realizar empréstimo");
-            System.out.println("3. Realizar devolução");
-            System.out.println("4. Logout");
-            int opcao = sc.nextInt();
-            switch (opcao) {
-                case 1:
-                    //consultaObras();
-                    break;
-                case 2:
-                    //emprestimo();
-                    break;
-                case 3:
-                    //devolucao();
-                    break;
-                case 4:
-                    //logout();
-                    break;
-                default:
-                    System.err.println("Opção inválida!");
-
-            }
-        }
-    }
-
     private void login(Scanner scanner) {
         System.out.println("Digite seu email: ");
         String email = scanner.next();
@@ -73,9 +45,9 @@ public class Biblioteca {
 
             Usuario u = usuarios.get(i); //pegue o usuário do ArrayList
             if (u.getEmail().equals(email) && u.getSenha().equals(senha) ) { //verifique email e senha
-                usuarioLogado = usuarios; //armazene o usuário logado
+
                 System.out.println("Bem-vindo, " + u.getNome() + "!");
-                //menuUsuario(scanner);
+                usuarios.get(i).menu(u);
                 return;
             }
         }
@@ -83,34 +55,24 @@ public class Biblioteca {
 
     }
 
-    private void menuUsuario(Scanner scanner) {
-
-    }
 
     private void carregarDados() {
         // Carregar dados de obras a partir do arquivo CSV
-        String caminhoArquivo = "acervo.csv";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
-            String linha;
-            while ((linha = br.readLine()) != null) {
-                String[] campos = linha.split(",");
-
-                // Ignorar cabeçalho
-                if (campos[0].equalsIgnoreCase("ID")) {
-                    continue;
-                }
-
-                String id = campos[0].trim();
-                String titulo = campos[1].trim();
-                String quantDisponivel = campos[2].trim();
-
-                obras.add(new Obra(id, titulo, quantDisponivel));
+        String arquivo= "acervo.cvc";
+        try (BufferedReader reader =new BufferedReader(new FileReader(arquivo))){
+            String linha = reader.readLine();
+            while (linha != null) {
+                String pedacosLinha[] = linha.split(",");
+                //String id, String titulo, String autor, String quantDisponivel
+                obras.add(new Obra(pedacosLinha[0],pedacosLinha[1],pedacosLinha[2]));
             }
-            System.out.println("Dados das obras carregados com sucesso!");
-        } catch (IOException e) {
-            System.out.println("Erro ao carregar dados de obras: " + e.getMessage());
+
+
+        }catch(IOException e){
+            System.out.println("Arquivo corrompido");
         }
+
+
     }
 }
 
--- src/Obra.java
+++ src/Obra.java
@@ -1,13 +1,12 @@
 public class Obra {
     private String id;
     private String titulo;
-    private String autor;
+   // private String autor;
     private String quantDisponivel;
 
-    public Obra(String id, String titulo, String autor, String quantDisponivel) {
+    public Obra(String id, String titulo, String quantDisponivel) {
         this.id = id;
         this.titulo = titulo;
-        this.autor = autor;
         this.quantDisponivel = quantDisponivel;
     }
 
@@ -19,19 +18,9 @@ public class Obra {
         return titulo;
     }
 
-    public String getAutor() {
-        return autor;
-    }
-
     public String getQuantDisponivel() {
         return quantDisponivel;
     }
 
-    @Override
-    public String toString() {
-        return "ID ='" + id + '\'' +
-                ", Título ='" + titulo + '\'' +
-                ", Autor ='" + autor + '\'' +
-                ", Quantidade disponível =" + quantDisponivel;
-    }
+
 }
